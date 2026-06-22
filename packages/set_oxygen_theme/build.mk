ac897612d60c6320d56ee5d6298d613f989e190e
dulstack <doowew100@gmail.com>
2026-06-22 10:06:36 +0200

    Removed the cosmic package from Readme 	modified:   packages/set_oxygen_theme/build.mk 	deleted:    packages/set_oxygen_theme/disk.img 	new file:   packages/set_oxygen_theme/root/DEBIAN/control 	new file:   packages/set_oxygen_theme/root/DEBIAN/copyright 	renamed:    packages/set_oxygen_theme/root/etc/systemd/system/set_oxygen_theme.service -> packages/set_oxygen_theme/root/usr/lib/systemd/system/set_oxygen_theme.service 	new file:   packages/set_oxygen_theme/root/usr/share/doc/set-oxygen-theme/copyright

--- packages/set_oxygen_theme/build.mk
+++ packages/set_oxygen_theme/build.mk
@@ -1,13 +1,18 @@
 CURDIR = set_oxygen_theme
 .PHONY: set_oxygen_theme-all set_oxygen_theme-build set_oxygen_theme-install
 
+.NOTPARALELL: 
 set_oxygen_theme-all: set_oxygen_theme-build set_oxygen_theme-install
 set_oxygen_theme-build: $(CHROOT)/usr/local/bin/set_oxygen_theme.elf
+
 set_oxygen_theme-install:
-	mkdir -p $(CHROOT)/etc/systemd/system
+	mkdir -p $(CHROOT)/usr/lib/systemd/system $(CHROOT)/../includes.binary
 	cd $(CURDIR);\
-	 cp root/etc/systemd/system/set_oxygen_theme.service \
-	  $(CHROOT)/etc/systemd/system/
+	 cp root/usr/lib/systemd/system/set_oxygen_theme.service \
+	  $(CHROOT)/usr/lib/systemd/system/;\
+	 cp -r root/DEBIAN $(CHROOT)/
+	dpkg-deb --root-owner-group --build $(CHROOT)/ \
+	 $(CHROOT)/../includes.binary/set-oxygen-theme.udeb
 
 $(CHROOT)/usr/local/bin/set_oxygen_theme.elf: $(CURDIR)/set_oxygen_theme.c
 	mkdir -p $(CHROOT)/usr/local/bin
